0b3945bdc7f4e9a3b9f864595c5e4510d1cea9b6
Mika Andrianarijaona <mikaoelitiana@gmail.com>
2024-08-03 15:42:43 +0200

    fix: remove post

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,5 @@
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 import { transactionRouter } from "~/server/api/routers/transaction";
-import { postRouter } from "~/server/api/routers/post";
 
 /**
  * This is the primary router for your server.
@@ -8,7 +7,6 @@ import { postRouter } from "~/server/api/routers/post";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   transaction: transactionRouter,
 });
 

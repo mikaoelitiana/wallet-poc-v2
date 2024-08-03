49634b1d3a9c59b4393fb72e6e1d6ac37643edb0
Mika Andrianarijaona <mikaoelitiana@gmail.com>
2024-08-03 15:31:08 +0200

    fix: remove post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,18 @@ import { api, HydrateClient } from "~/trpc/server";
 import { Transactions } from "./_components/transaction";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-  await new Promise((resolve) => setTimeout(resolve, 100));
+  // const hello = await api.post.hello({ text: "from tRPC" });
+  // await new Promise((resolve) => setTimeout(resolve, 100));
+  await api.transaction.getTransactions.prefetch();
 
-  void api.transaction.getTransactions.prefetch();
+  // void api.transaction.getTransactions.prefetch();
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+      <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        <div className="container flex flex-col items-center gap-6 px-1 py-4">
           <h1 className="text-4xl font-extrabold tracking-tight">My Wallet</h1>
           <Transactions />
-
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello ? hello.greeting : "Loading tRPC query..."}
-            </p>
-          </div>
         </div>
       </main>
     </HydrateClient>
